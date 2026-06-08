47eb14830c06f73713dd526d23da1b5211e510ab
Carol <caroldazevedo52@gmail.com>
2026-06-08 16:37:58 +0000

    :heavy_plus_sign:build: correção e salvamento dos dados no banco local

--- compras/src/app/components/listar/listar.ts
+++ compras/src/app/components/listar/listar.ts
@@ -1,48 +1,23 @@
-import { Component, Input, signal, Signal, WritableSignal } from '@angular/core';
-import { form, required, min } from '@angular/forms/signals';
+import { Component, Input, WritableSignal } from '@angular/core';
+import { NgForOf } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 
-import { ProdutoData, ProdutoModel } from '../../app.form';
+import { ProdutoData } from '../../app.form';
 import { Alterar } from '../alterar/alterar';
 import { Deletar } from '../deletar/deletar';
 
 @Component({
   standalone: true,
   selector: 'app-listar',
-  imports: [
-    Alterar, 
-    Deletar, 
-    ButtonModule
-  ],
+  imports: [NgForOf, Alterar, Deletar, ButtonModule],
   templateUrl: './listar.html',
   styleUrl: './listar.css',
 })
 export class Listar {
   @Input() produtos!: WritableSignal<ProdutoData[]>;
-  protected readonly produtoEditando = signal<ProdutoData | null>(null);
-  protected readonly ProdutoModel = signal<ProdutoData>({ ...ProdutoModel });
-  protected readonly ProdutoForm = form(this.ProdutoModel, (produto) => {
-    required(produto.nome, { message: 'Um nome é necessário' });
-    min(produto.quantidade, 0, { message: 'A quantidade não pode ser negativa' });
-  });
-
-  AlterarProduto(produto: ProdutoData) {
-    this.produtoEditando.set(produto);
-    this.ProdutoModel.set({ ...produto });
-  }
 
   DeletarProduto(produto: ProdutoData) {
     this.produtos.update((lista: ProdutoData[]) => lista.filter((item: ProdutoData) => item !== produto));
-    
-    if (this.produtoEditando() === produto) {
-      this.CancelarEdicao();
-    }
-  }
-
-  CancelarEdicao() {
-    this.produtoEditando.set(null);
-    this.ProdutoModel.set({ ...ProdutoModel });
-    this.ProdutoForm().reset();
   }
 }
 
